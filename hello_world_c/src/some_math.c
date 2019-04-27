627a3c50066f45aaf5a5f576df0989b63209082c
Tom Moody <tom.moody@gmail.com>
2019-04-27 08:05:26 -0600

    New commit

---
+++ hello_world_c/src/some_math.c
@@ -0,0 +1,14 @@
+/*
+ * some_math.c
+ *
+ *  Created on: Jan 19, 2019
+ *      Author: tommoody
+ */
+
+#include <stdio.h>
+#include "some_math.h"
+
+int add(x,y){
+
+	return x + y;
+}
---
+++ hello_world_c/src/some_math.h
@@ -0,0 +1,6 @@
+#ifndef SOME_MATH_H
+#define SOME_MATH_H
+
+int add(int x,int y);
+
+#endif
